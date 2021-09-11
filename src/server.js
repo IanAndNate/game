dc2d535fbcbab652f7a83cc92cab8435f3786af5
Nate Mowbray <nmowbray@atlassian.com>
2021-09-11 13:55:57 +1000

    init

---
+++ src/server.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+
+const app = express();
+
+app.use(express.static(`src/static`));
+app.use(express.static(`node_modules`));
+
+const server = createServer(app);
+const io = new Server(server);
+
+server.listen(3000, () => {
+    console.log('listening on *:3000');
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+});
