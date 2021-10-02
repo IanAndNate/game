7cf0df22cbf07b3164abd45b2eded7b9b09b98b7
Nate Mowbray <nmowbray@atlassian.com>
2021-10-02 14:32:10 +1000

    fix issues with deployment

---
+++ converter.js
@@ -0,0 +1,18 @@
+import Midi from '@tonejs/midi';
+import { readFile, writeFile } from 'fs/promises';
+// load a midi file in the browser
+readFile('midi/amazgrac04.mid').then((buffer) => {
+    const midi = new Midi.Midi(buffer);
+    console.log(midi);
+    writeFile('midi/amazgrac04.json', JSON.stringify(midi));
+});
+readFile('midi/2289444_1.mid').then((buffer) => {
+    const midi = new Midi.Midi(buffer);
+    console.log(midi);
+    writeFile('midi/2289444_1.json', JSON.stringify(midi));
+});
+readFile('midi/tetris.mid').then((buffer) => {
+    const midi = new Midi.Midi(buffer);
+    console.log(midi);
+    writeFile('midi/tetris.json', JSON.stringify(midi));
+});
