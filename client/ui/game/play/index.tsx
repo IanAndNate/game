cdaa13ced3da62d1002f6f9e9b8a33d04f9a7a99
Nate Mowbray <nmowbray@atlassian.com>
2021-10-03 16:16:05 +1100

    update view to emotion

--- client/ui/game/play/index.tsx
+++ client/ui/game/play/index.tsx
@@ -1,94 +1,98 @@
 import React from 'react';
-import { useGame, useKeyboard } from "../../../controllers/game"
+import { useGame, useKeyboard } from '../../../controllers/game'
 import { GameStatus } from '../../../controllers/game/types';
 import { Room } from '../../common/room';
+import styled from '@emotion/styled';
+
+const MusicContainer = styled('div')`
+    height: 500px;
+    overflow: hidden;
+    perspective: 1000px;
+    position: relative;
+    display: flex;
+    justify-content: center;
+    perspective-origin: bottom;
+`;
+
+const MusicPage = styled.div<{ duration: number; numberNotes: number; started: boolean; }>`
+    position: absolute;
+    height: ${({ duration }) => duration * 500}px;
+    width: ${({ numberNotes }) => numberNotes * 30}px;
+    transform: rotateX(70deg);
+    bottom: 0;
+    transform-origin: bottom;
+    transition: transform ${({ duration }) => duration}s linear 0s;
+    transform: rotateX(71deg) ${({ started, duration }) => started ? 'translate3d(0, ' + duration * 500 + 'px, 0)' : ''};
+`;
+
+const NoteBar = styled('div')`
+    position: relative;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
+const Note = styled.span<{ time: number; duration: number; note: string; }>`
+    position: absolute;
+    padding: 10px;
+    box-sizing: border-box;
+    width: 20px;
+    bottom: ${({ time }) => time * 500}px;
+    height: ${({ duration }) => duration * 500}px;
+    background-color: #93d793;
+    left: ${({ note }) => getPosition(note) * 30}px;
+`;
+
+const UserNote = styled.button`
+    width: 20px;
+    margin-right: 10px;
+`;
 
 export const Play = () => {
     const [{ latency, timeDiff, piece, status, players }, { mouseDown, mouseUp }] = useGame();
     useKeyboard();
 
     const { song, notes } = piece || { song: null, piece: null };
-    const lastNote = song && song[song.length - 1];
+    const lastNote = song && song[ song.length - 1 ];
     const duration = lastNote && (lastNote.time + lastNote.duration);
     const numberNotes = notes && notes.length;
 
-    return  <>
+    return <>
         <p>latency: {latency}ms</p>
         <p>timeDiff: {timeDiff}ms</p>
         <Room players={players} disabled={false}/>
-        <style>
-            {`.started .musicPage {
-                transform: rotateX(71deg) translate3d(0, ${duration * 500}px, 0);
-            }
-            
-            .musicContainer {
-                height: 500px;
-                overflow: hidden;
-                perspective: 1000px;
-                position: relative;
-                display: flex;
-                justify-content: center;
-                perspective-origin: bottom;
-            }
-            
-            .musicPage {
-                position: absolute;
-                height: ${duration * 500}px;
-                width: ${numberNotes * 30}px;
-                transform: rotateX(71deg);
-                bottom: 0;
-                transform-origin: bottom;
-                transition: transform ${duration}s linear 0s;
-            }
-            
-            .note {
-                position: absolute;
-                padding: 10px;
-                box-sizing: border-box;
-                width: 20px;
-            }
-            
-            .noteBar {
-                position: relative;
-                display: flex;
-                align-items: center;
-                justify-content: center;
-            }
-            
-            .displayNote {
-                width: 20px;
-                margin-right: 10px;
-            }
-`}
-        </style>
-        <div className={status === GameStatus.Running ? 'musicContainer started': 'musicContainer'}>
-            <div className={'musicPage'}>{song && song.filter(({ key }) => !!key).map(({key, time, duration}, i) => <span className={'note'} key={i} style={{
-                bottom: time * 500,
-                height: duration * 500,
-                backgroundColor: !key ? 'rgb(215 169 147)': '#93d793',
-                left: getPosition(key) * 30,
-            }}></span>)}</div>
-        </div>
-        <div className={'noteBar'}>
+        <MusicContainer>
+            <MusicPage numberNotes={numberNotes}
+                       duration={duration}
+                       started={status === GameStatus.Running}>
+                {song && song
+                    .filter(({ key }) => !!key)
+                    .map(({ key, time, duration }, i) =>
+                        <Note key={i}
+                              time={time}
+                              note={key}
+                              duration={duration}/>
+                    )}
+            </MusicPage>
+        </MusicContainer>
+        <NoteBar>
             {notes && notes.map(({ key }, i) =>
-                <button value={key} onMouseDown={mouseDown} onMouseUp={mouseUp} className={'displayNote'} key={i} style={{
-                    left: getPosition(key) * 40,
-                }}>{key}</button>
+                <UserNote value={key} onMouseDown={mouseDown} onMouseUp={mouseUp} key={i}>{key}</UserNote>
             )}
-        </div>
+        </NoteBar>
     </>
 }
 
 let nextPosition = 0;
 
-const NOTE_POSITIONS:{ [note: string]: number } = {}
+const NOTE_POSITIONS: { [ note: string ]: number } = {}
 
 const getPosition = (note: string) => {
-    const existing = NOTE_POSITIONS[note];
+    const existing = NOTE_POSITIONS[ note ];
     if (typeof existing !== 'undefined') {
         return existing;
     }
-    NOTE_POSITIONS[note] = nextPosition;
+    NOTE_POSITIONS[ note ] = nextPosition;
     nextPosition++;
-    return NOTE_POSITIONS[note];
+    return NOTE_POSITIONS[ note ];
 }
